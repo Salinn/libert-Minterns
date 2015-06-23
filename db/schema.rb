c68821126bf2cdac9ee5ecc042226fd0a1c3db6c
Paul Darragh <pmd6624@rit.edu>
2015-06-23 20:36:56 +0000

    4 tables migrated succesfully

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150623202340) do
+ActiveRecord::Schema.define(version: 20150623203440) do
 
   create_table "colleges", force: :cascade do |t|
     t.string   "name"
@@ -36,6 +36,17 @@ ActiveRecord::Schema.define(version: 20150623202340) do
     t.datetime "updated_at",     null: false
   end
 
+  create_table "galleries", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "event_id"
+    t.integer  "photo_challenge_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
+
+  add_index "galleries", ["event_id"], name: "index_galleries_on_event_id"
+  add_index "galleries", ["photo_challenge_id"], name: "index_galleries_on_photo_challenge_id"
+
   create_table "majors", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -51,6 +62,30 @@ ActiveRecord::Schema.define(version: 20150623202340) do
 
   add_index "managers", ["department_id"], name: "index_managers_on_department_id"
   add_index "managers", ["user_id"], name: "index_managers_on_user_id"
+
+  create_table "photo_challenges", force: :cascade do |t|
+    t.string   "title"
+    t.text     "theme"
+    t.integer  "year_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "photo_challenges", ["year_id"], name: "index_photo_challenges_on_year_id"
+
+  create_table "photos", force: :cascade do |t|
+    t.string   "picture"
+    t.text     "comment"
+    t.integer  "gallery_id"
+    t.integer  "photo_challenge_id"
+    t.integer  "winning_photo_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
+
+  add_index "photos", ["gallery_id"], name: "index_photos_on_gallery_id"
+  add_index "photos", ["photo_challenge_id"], name: "index_photos_on_photo_challenge_id"
+  add_index "photos", ["winning_photo_id"], name: "index_photos_on_winning_photo_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -68,6 +103,15 @@ ActiveRecord::Schema.define(version: 20150623202340) do
   add_index "users", ["college_id"], name: "index_users_on_college_id"
   add_index "users", ["department_id"], name: "index_users_on_department_id"
   add_index "users", ["major_id"], name: "index_users_on_major_id"
+
+  create_table "winning_photos", force: :cascade do |t|
+    t.integer  "photo_challenge_id"
+    t.text     "reason"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
+
+  add_index "winning_photos", ["photo_challenge_id"], name: "index_winning_photos_on_photo_challenge_id"
 
   create_table "years", force: :cascade do |t|
     t.string   "name"
