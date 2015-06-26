0672d798d854e7e3dbb57851ba98baeceac38c4a
Paul Darragh <pmd6624@rit.edu>
2015-06-26 19:22:27 +0000

    event photos work now

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+   get 'show_event_photos', to: 'events#show_event_photos', as: :show_event_photos
+  get 'event_photos/:event_id' => 'events#event_photos', as: 'event_photos'
+  get 'photo_challenge_gallery/:photo_challenge_id' => 'photo_challenges#photo_challenge_gallery', as: :photo_challenge_gallery
+  
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
   
   resources :intern_summaries
