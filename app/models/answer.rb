f74c96d5840ed84f2f8d834e53883546e8b83062
salinn <pmd6624@rit.edu>
2015-07-10 20:44:45 -0400

    updated faq page ui

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,10 @@ class Answer < ActiveRecord::Base
   belongs_to :user
   belongs_to :rating
   belongs_to :question
+
+  after_create :create_rating
+
+  def create_rating
+    self.update(rating_id: Rating.create!(faq_id: self.question.faq.id).id)
+  end
 end
