58805fbb486d948d1aadacd09feffd907d3287d3
Paul Darragh <pmd6624@rit.edu>
2015-07-28 20:43:50 +0000

    updated photo challenge upload and how to get to upload

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -21,7 +21,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   #
-    'missisng_image.jpg'
+    'missing_image.jpg'
   end
 
   # Process files as they are uploaded:
