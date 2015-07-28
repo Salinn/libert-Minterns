b309120f4724f4fbbed664eaec08cdd00674c94c
Paul Darragh <pmd6624@rit.edu>
2015-07-28 20:43:50 +0000

    updated photo challenge upload and how to get to upload

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -34,7 +34,7 @@ class PhotosController < ApplicationController
 
     respond_to do |format|
       if @photo.save
-        format.html { redirect_to @photo, notice: 'Photo was successfully created.' }
+        format.html { redirect_to URI(request.referrer).path, notice: 'Photo was successfully created.' }
         format.json { render :show, status: :created, location: @photo }
       else
         format.html { render :new }
